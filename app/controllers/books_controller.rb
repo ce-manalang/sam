4bd36ae108838a6c972bf6096e8de4bde0a0fc22
ce <cecillemanalang@gmail.com>
2026-02-14 15:32:57 +0800

    feat: book detail page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,34 @@
 class BooksController < ApplicationController
   # This skips auth for the public list
-  before_action :authenticate_user!, except: [ :index ]
+  before_action :authenticate_user!, except: [ :index, :show ]
 
   def index
     render json: DatoCmsService.fetch_books
+  end
+
+  def show
+    dato_book = DatoCmsService.fetch_book(params[:id])
+
+    if dato_book
+      response_data = dato_book.as_json
+
+      if current_user
+        library_book = current_user.library_books.find_by(dato_book_id: params[:id])
+        if library_book
+          response_data = response_data.merge(
+            library_status: library_book.status,
+            library_notes: library_book.notes,
+            in_library: true
+          )
+        else
+          response_data[:in_library] = false
+        end
+      end
+
+      render json: response_data
+    else
+      render json: { error: "Book not found" }, status: :not_found
+    end
   end
 
   def my_catalog
--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -34,4 +34,61 @@ RSpec.describe "Books", type: :request do
       expect(salary).to have_key("cover")
     end
   end
+
+  describe "GET /books/:id" do
+    let(:book_id) { "ex7XI1oCTES8Zt9i1TspUA" }
+    let(:mock_dato_book) do
+      {
+        "id" => book_id,
+        "title" => "Mr Salary",
+        "author" => "Sally Rooney",
+        "isbn" => "1234567890",
+        "description" => "A short story",
+        "cover" => { "url" => "http://example.com/cover.jpg" },
+        "tags" => "Fiction, Short Story"
+      }
+    end
+
+    before do
+      allow(DatoCmsService).to receive(:fetch_book).with(book_id).and_return(mock_dato_book)
+    end
+
+    context "when unauthenticated" do
+      it "returns book metadata" do
+        get "/books/#{book_id}"
+
+        expect(response).to have_http_status(:ok)
+        json = JSON.parse(response.body)
+        expect(json["title"]).to eq("Mr Salary")
+        expect(json).not_to have_key("in_library")
+      end
+    end
+
+    context "when authenticated" do
+      before do
+        post user_session_path, params: { user: { email: user.email, password: user.password } }, headers: { "Accept" => "application/json" }
+        @token = response.headers['Authorization']
+      end
+
+      it "returns book metadata with in_library: true if in library" do
+        LibraryBook.create!(user: user, dato_book_id: book_id, status: "read", notes: "Great book")
+
+        get "/books/#{book_id}", headers: { "Authorization" => @token }
+
+        expect(response).to have_http_status(:ok)
+        json = JSON.parse(response.body)
+        expect(json["in_library"]).to be true
+        expect(json["library_status"]).to eq("read")
+        expect(json["library_notes"]).to eq("Great book")
+      end
+
+      it "returns book metadata with in_library: false if not in library" do
+        get "/books/#{book_id}", headers: { "Authorization" => @token }
+
+        expect(response).to have_http_status(:ok)
+        json = JSON.parse(response.body)
+        expect(json["in_library"]).to be false
+      end
+    end
+  end
 end
